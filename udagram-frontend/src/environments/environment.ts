3e12f1433f20a92e759d19a0df14f88839a1388a
TaiNgo30 <ngoductai2004@gmail.com>
2024-09-21 04:32:41 +0700

    final commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae404140a2e8f46129bcda98de94f25f-631884324.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
